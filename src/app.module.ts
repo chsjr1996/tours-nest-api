bbc923c3e6f12b948fc2dc46c409a866e57d1417
Carlos H <chsjr1996@gmail.com>
2021-02-02 00:44:17 -0300

    Remove unnecessary controller
    - app.controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReviewController } from './controllers/review/review.controller';
+import { TourController } from './controllers/tour/tour.controller';
+import { UserController } from './controllers/user/user.controller';
+import { AuthController } from './controllers/auth/auth.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [
+    ReviewController,
+    TourController,
+    UserController,
+    AuthController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}

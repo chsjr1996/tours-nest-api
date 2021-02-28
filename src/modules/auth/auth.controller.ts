061ed392fb7af21602311f11516ddf836a1c6f11
Carlos H <chsjr1996@gmail.com>
2021-02-28 18:02:54 -0300

    Fix auth controller
    - Made login route public

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { ApiTags, ApiBody } from '@nestjs/swagger';
 import { Request } from 'express';
+import { Public } from 'src/common/decorators/public/public.decorator';
 import { UserDTO } from '../user/dto/user.dto';
 import { AuthService } from './auth.service';
 import { LoginDTO } from './dto/login.dto';
@@ -12,6 +13,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @ApiBody({ type: LoginDTO })
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   public async login(@Req() req: Request) {

9a655c132c7d4a1094d513b4685b5de4ce688670
Carlos H <chsjr1996@gmail.com>
2021-02-28 18:02:00 -0300

    Enable Nest App CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import Filters from './common/exception-filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(...Filters);
